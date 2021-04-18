240a02f030770a3b5bba30b6a76e870df82a969a
Naman <cool.naman.ajmera@gmail.com>
2021-04-18 10:30:58 +0530

    Added: EmployeeWageUC2.java and Modified: .gitignore

---
+++ EmployeeWageUC2.java
@@ -0,0 +1,18 @@
+class EmployeeWageUC2 {
+    static final int isPresent = 1;
+    static final int empPerRate = 20;
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+
+        // System.out.println();
+        int empPerHour = 0;
+        int empWage = 0;
+        if (empCheck == isPresent) {
+            empPerHour = 8;
+        }
+        empWage = empPerHour * empPerRate;
+        System.out.println("Emp Wage:- " + empWage);
+    }
+}
